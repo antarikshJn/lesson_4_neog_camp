94e0bcc7021f3182f583831acfca831cd1196c30
antarikshJn <97785031+antarikshJn@users.noreply.github.com>
2022-01-28 22:45:51 +0530

    Heading Color change with Js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("Hey your External Javascript is woring")
+var headingColor = prompt( "which color do you want for headings");
+var allHeadings = document.querySelectorAll("h1");
+var btn = document.querySelector("button");
+btn.addEventListener("click", colorFunction);
+function colorFunction(){
+    console.log("headingColor : ", headingColor);
+    for (let index = 0; index < allHeadings.length; index++) {
+    allHeadings[index].style.color = headingColor;   
+    }
+
+}
+console.log(headingColor,allHeadings,btn);
